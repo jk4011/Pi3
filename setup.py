00a0b277e61d77fd8ea815646cc9f662d74fe065
JinHyeok <jinhyeok@unist.ac.kr>
2025-10-23 04:32:38 +0000

    make pi3 installable

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name="pi3",
+    version="0.0.1",
+    description="pi3",
+    long_description_content_type="text/markdown",
+    packages=["pi3"],
+    python_requires=">=3.6, <4",
+    project_urls={
+        "Source": "https://github.com/jk4011/Pi3",
+    },
+)
